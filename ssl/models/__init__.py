2c79d75bb8a0d60c51e34d8365cc9237096a9099
inoue26 <inoue@araya.org>
2023-07-31 10:50:37 +0900

    MOD: fill path

--- ssl/models/__init__.py
+++ ssl/models/__init__.py
@@ -1,13 +1,13 @@
 from .sc_mbm import SCMBMTrainer, MAEForEEG
 
-def get_model_and_trainer(config, device_count=1, usewandb=True):
+def get_model_and_trainer(config, device_count=1, usewandb=True, only_model:bool=False):
     model_name = config.model.name
     model_config = config.model.parameters
     training_config = config.training
     model_name = model_config.name
     if model_name == 'sc_mbm':
         model = MAEForEEG(**model_config)
-        trainer = SCMBMTrainer(training_config, device_count, usewandb)
+        trainer = SCMBMTrainer(training_config, device_count, usewandb) if not only_model else None
     else:
         raise ValueError('model_config.name {} is not supported'.format(model_config.name))
 
--- train_ssl.py
+++ train_ssl.py
@@ -1,20 +1,80 @@
-
+import argparse
+from hydra import compose, initialize
+import sys
+sys.path.append('.')
+from ssl.dataclass import parse_dataset
+from ssl.models import get_model_and_trainer
+from omegaconf import OmegaConf
+import wandb
 
 # get dataset
-
+def get_dataset(cfg:OmegaConf):
+    dataset_names:dict = cfg.dataset
+    dataset_yamls:dict = cfg.dataset_yaml
+    num_trial_limit:dict = cfg.total_limit
+    preproc_config:OmegaConf = cfg.preprocess
+    h5_root:str = cfg.h5_root
+    image_preprocs:list = []
+    meg_preprocs:list = []
+    only_meg:bool = True
+    on_memory:bool = False
+    dataset_dict:dict = parse_dataset(dataset_names, dataset_yamls, preproc_config, num_trial_limit, 
+                                      h5_root, image_preprocs, meg_preprocs, only_meg, on_memory)
+    
+    return dataset_dict['train'], dataset_dict['val']
+    
 
 # get model
+def get_model(config, usewandb, devuce_count):
+    model, trainer = get_model_and_trainer(config, device_count=devuce_count, usewandb=usewandb, only_model=False)
+    return model. trainer
 
-
-# get trainer
 
 
 # run
+def run(cfg:OmegaConf, wandb_key_path:str, device_counts:int):
+    # wandb setting
+    if wandb_key_path is not None:
+        usewandb=True
+        with open(wandb_key_path, 'r') as f:
+            wandb_key = f.read()
+            wandb_key = wandb_key.split('\n')[0]
+        wandb.login(key = f'{wandb_key}')
+        # wandb.init(project='llm_hackason-'+args.config)
+        # wandb.init(project='llm_hackason-new_prompt')
+        wandb.init(project='ssl-meg-movie')        
+    else:
+        usewandb=False
+    print('============================ settings ==============================')
+    print(OmegaConf.to_yaml(cfg))
+    print('=============================== END ================================')
+    # dataset
+    train_dataset, val_dataset = parse_dataset(cfg)
+
+    # model
+    model, trainer = get_model_and_trainer(cfg, usewandb, device_counts)
+
+    trainer.fit(model, train_dataset, val_dataset, ckpt_path=cfg.resume_path)
 
 
 # argparse
+def parse_args():
+    parser = argparse.ArgumentParser(
+                        prog='Self-Supervised learning for MEG-image decoding',
+                        description='with Yanagisawa-Lab',
+                        epilog='Created by inoue@araya.org')
+    parser.add_argument('--config', type=str, default='ssl/ssl_configs/test_config.yaml')
+    parser.add_argument('--resume', type=str, default=None)
+    parser.add_argument('--wandbkey', type=str, default=None) # default='/home/yainoue/wandb_inoue.txt')
+    parser.add_argument('device_counts', type=int, default=1)
 
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+    return args
 
 
 if __name__ == '__main__':
-    pass
+    args = parse_args()
+    initialize(config_path='ssl/ssl_configs/')
+    cfg = compose(args.config, args.wandbkey, args.device_counts)
+    cfg.resume_path = args.resume
